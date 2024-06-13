77c8cc8e9d0de5b31eeebd95c0c1a52fc70e0880
Anurag Singh <anuragsinghjdtech@gmail.com>
2024-06-13 15:37:30 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const cors= require('cors');
 const connectDB = require('./db');
+const saveFormDataRouter = require('./saveFormData');
 const app= express();
 app.use(cors());
 app.use(express.json());
@@ -9,7 +10,7 @@ connectDB();
 app.get('/',(req,res)=>{
     res.send("hellow world")
 })
-app.use('/saveLeadData',require('./saveFormData'))
+app.use('/saveLeadData', allowCors(saveFormDataRouter));
 
 app.listen(8000,()=>{
     console.log("listining to port 8000")
